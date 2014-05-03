70c639a362894516694a9e935e6e247feb7b1590
xtofl <xtofl@github.com>
2014-05-03 20:25:17 +0200

    [impl] match checking

--- memory.js
+++ memory.js
@@ -1,23 +1,33 @@
 //require jquery
 var memory = function(){
+	var current_up = null;
 	var states = {
 		begin: { 
 			started: function(){
-				state = states.wait_clicked;
+				console.log("begin: started -> wait_turn_first");
+				state = states.wait_turn_first;
 			} },
-		wait_clicked: {
-			clicked: function(){
-				state = states.wait_clicked_again;
+		wait_turn_first: {
+			turn: function(tile){
+				console.log("wait_turn_first: turn -> wait_turn_second");
+				state = states.wait_turn_second;
+				current_up = tile;
 			}
 		},
-		wait_clicked_again: {
-			clicked: function(){
-				state = states.wait_clicked;
+		wait_turn_second: {
+			turn: function(tile){
+				console.log("wait_turn_second: turn -> ...");
+				state = states.wait_turn_first;
+				if (tile.matches(current_up)) {
+					console.log("match");
+				} else {
+					console.log("no match");
+				}
 			}
 		},
 		won: ""
 	};
-	var state = states.wait_clicked;
+	var state = states.begin;
 
 	var createImage = function(i){
 		var img = document.createElement("img");
@@ -37,11 +47,16 @@ var memory = function(){
 		var tile = document.createElement("div");
 		var img = createImage(i);
 		tile.appendChild(img);
+		tile.img = img;
 		var back = createBack(i);
 		tile.appendChild(back);
 		tile.setAttribute("class", "face-down");
+		tile.matches = function(other){
+			return tile.img === other.img;
+		};
 		var up = function(){
 			tile.setAttribute("class", "face-up");
+			state.turn(tile);
 			next = down;
 		};
 		var down = function(){
