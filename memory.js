599676c37251f1707adcf9f89156b1e9d0c41133
xtofl <xtofl@github.com>
2014-05-03 20:04:13 +0200

    [impl] read images from builder

--- memory.js
+++ memory.js
@@ -18,9 +18,39 @@ var memory = function(){
 		won: ""
 	};
 	var state = states.wait_clicked;
-	return {
+
+	var createImage = function(i){
+		var img = document.createElement("img");
+		img.setAttribute("src", "images/butterfly-0"+(i+1)+".jpg");
+		img.setAttribute("class", "front");
+		return img;
+	};
+
+	var createBack = function(i){
+		var img = document.createElement("img");
+		img.setAttribute("src", "images/back.jpg");
+		img.setAttribute("class", "back");
+		return img;
+	};
+
+	var createTile = function(i){
+		var tile = document.createElement("div");
+		tile.appendChild(createImage(i));
+		tile.appendChild(createBack(i));
+		img.setAttribute("class", "face-down");
+		return tile;
+	};
+
+	var memory = {
+		build: function(element){
+			       for (var i=0; i != 9; ++i) {
+				       var img = createImage(i);
+				       element.append(img);
+			       }
+		},
 		start: function(){ 
 			state.started();
 		}
 	};
+	return memory;
 }();
